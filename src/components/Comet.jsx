645194767f8fec4cac270574f588dc3649182514
Ian Flynn <ianflynnwork@gmail.com>
2023-05-22 17:19:10 -0400

    simplified comet component

--- src/components/Comet.jsx
+++ src/components/Comet.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 
 const Comet = ({ type, time, changeTime, floatDirection }) => {
   return (
-    <div>
-      <figure className={type + '-stripe'}>
-        <div className={'orb orb-contents ' + floatDirection}>
-          <h3 id={type + '-label'}>{type[0].toUpperCase() + type.slice(1)}</h3>
-          <div>
-            <button onClick={() => changeTime(-60, type)}>-</button>
-            <span id={type + '-length'}> {time / 60} </span>
-            <button onClick={() => changeTime(60, type)}>+</button>
-          </div>
+    <div className={type + '-stripe'}>
+      <div className={'orb orb-contents ' + floatDirection}>
+        <h3 id={type + '-label'}>{type[0].toUpperCase() + type.slice(1)}</h3>
+        <div>
+          <button onClick={() => changeTime(-60, type)}>-</button>
+          <span id={type + '-length'}> {time / 60} </span>
+          <button onClick={() => changeTime(60, type)}>+</button>
         </div>
-      </figure>
+      </div>
     </div>
   );
 };
