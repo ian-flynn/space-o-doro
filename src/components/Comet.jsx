85526d28c6d5235e3afbe330ad86d6002a0d6068
Ian Flynn <ianflynnwork@gmail.com>
2023-05-22 16:26:21 -0400

    removed fontawesome dependancy for button icons. Just using words now

--- src/components/Comet.jsx
+++ src/components/Comet.jsx
@@ -2,28 +2,14 @@ import React from 'react';
 
 const Comet = ({ type, time, changeTime, floatDirection }) => {
   return (
-    <div id={type + '-box'}>
+    <div>
       <figure className={type + '-stripe'}>
         <div className={'orb orb-contents ' + floatDirection}>
-          <h3 id={type + '-label'}>
-            {type[0].toUpperCase() + type.slice(1)}
-            <br />
-            Length
-          </h3>
+          <h3 id={type + '-label'}>{type[0].toUpperCase() + type.slice(1)}</h3>
           <div>
-            <button
-              onClick={() => changeTime(-60, type)}
-              id={type + '-decrement'}
-            >
-              <i className='fa-solid fa-arrow-down fa-xl'>-</i>
-            </button>
+            <button onClick={() => changeTime(-60, type)}>-</button>
             <span id={type + '-length'}> {time / 60} </span>
-            <button
-              onClick={() => changeTime(60, type)}
-              id={type + '-increment'}
-            >
-              <i className='fa-solid fa-arrow-up fa-xl'>+</i>
-            </button>
+            <button onClick={() => changeTime(60, type)}>+</button>
           </div>
         </div>
       </figure>
