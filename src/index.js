e60876490b21c26517c1df6eb6b9ad4073e6e875
Ian Flynn <ianflynnwork@gmail.com>
2023-08-04 15:47:49 -0400

    postcss setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import App from './App.jsx';
-import './style.css';
+import '../build/styles/index.css';
 import * as ReactDOMClient from 'react-dom/client';
 const container = document.getElementById('root');
 const root = ReactDOMClient.createRoot(container);
