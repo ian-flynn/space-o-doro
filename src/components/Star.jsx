e7529235c76c6c264375cba889d112a77148b7e1
Ian Flynn <ianflynnwork@gmail.com>
2023-08-21 12:12:32 -0400

    style changes

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
+const starStyle = {};
 const Star = ({ star }) => {
-  return <div className={'four-point-star '} id={star}></div>;
+  return <div className={'four-point-star '} id={star} style={starStyle}></div>;
 };
 
 export default Star;
